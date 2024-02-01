ad4f4d3e7d036da16bf596ee94fcbc341c6069cd
sumitbhardwajjj <135961691+sumitbhardwajjj@users.noreply.github.com>
2024-02-01 19:28:18 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import Slidebar from './Slidebar'
 import SearchBar from './SearchBar'
 import styles from "../styles/Index.module.css"
-import Header from './Header'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
